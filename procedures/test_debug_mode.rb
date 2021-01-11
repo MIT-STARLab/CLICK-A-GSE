4a609751d9f26de18931fd03886756cc5ee7d0d6
Peter Grenfell <pgrenfel@mit.edu>
2021-01-11 13:16:58 -0500

    bus command scripts

--- procedures/test_debug_mode.rb
+++ procedures/test_debug_mode.rb
@@ -1,17 +1,13 @@
-#Test Script: PL_ECHO
+#Test Script - PL_CONFIG_OPTICAL_DOWNLINK
 
 ap_id = 15
 ap_id_type = "C"
 op_code = 3
 op_code_type = "C"
-cmd_length = 5
+cmd_length = 3
 cmd_length_type = "S>"
-rpi_apid = 0x3D
+rpi_apid = 0xD0
 rpi_apid_type = "C"
-payload_size = 1
-payload_size_type = "C"
-echo_data = 123
-echo_data_type = "C"
 
 xb1_cmd_data = []
 packing_directives = ""
@@ -23,10 +19,6 @@ xb1_cmd_data[2] = cmd_length
 packing_directives += cmd_length_type
 xb1_cmd_data[3] = rpi_apid
 packing_directives += rpi_apid_type
-xb1_cmd_data[4] = payload_size
-packing_directives += payload_size_type
-xb1_cmd_data[5] = echo_data
-packing_directives += echo_data_type
 
 xb1_cmd_data_packed = xb1_cmd_data.pack(packing_directives)
 xb1_cmd_data_unpacked = xb1_cmd_data_packed.unpack("C*")
@@ -38,7 +30,7 @@ puts xb1_cmd_data_packed
 puts xb1_cmd_data_unpacked
 puts ch_sum
 
-cmd("UUT PL_ECHO with ECHO_DATA #{echo_data}, CH_SUM #{ch_sum}")
-buffer = get_cmd_buffer("UUT","PL_ECHO")
+cmd("UUT PL_CONFIG_DEBUG with CH_SUM #{ch_sum}")
+buffer = get_cmd_buffer("UUT","PL_CONFIG_DEBUG")
 cmd_pkt = buffer.unpack("C*")
 puts cmd_pkt[14..cmd_pkt.length()]
