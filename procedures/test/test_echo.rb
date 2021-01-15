ee956f3c08d7b4c42aefb7e5c5ed746ebcee67e3
unknown <CLICK@AA-8VGW373.mit.edu>
2021-01-15 15:14:49 -0500

    fix echo bug

--- procedures/test/test_echo.rb
+++ procedures/test/test_echo.rb
@@ -6,11 +6,11 @@ load 'C:/BCT/71sw0078_a_cosmos_click_edu/procedures/lib/click_cmd.rb'
 
 #define data bytes
 data = []
-data[1] = 0x01
-data[2] = 0x02
-data[3] = 0x03
-data[4] = 0x04
-data[5] = 0x05
+data[0] = 0x01
+data[1] = 0x02
+data[2] = 0x03
+data[3] = 0x04
+data[4] = 0x05
 packing = "C*"
 
 #SM Send via UUT PAYLOAD_WRITE
