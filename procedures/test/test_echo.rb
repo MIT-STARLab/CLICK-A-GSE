95cad343f2a73205ed88f18a71bf25e6bfa324e5
unknown <CLICK@AA-8VGW373.mit.edu>
2021-01-15 15:17:43 -0500

    update echo

--- procedures/test/test_echo.rb
+++ procedures/test/test_echo.rb
@@ -4,14 +4,12 @@
 load 'C:/BCT/71sw0078_a_cosmos_click_edu/procedures/lib/pl_cmd_tlm_apids.rb'
 load 'C:/BCT/71sw0078_a_cosmos_click_edu/procedures/lib/click_cmd.rb'
 
+#echo data
+echo_data = 'HelloWorld'
 #define data bytes
 data = []
-data[0] = 0x01
-data[1] = 0x02
-data[2] = 0x03
-data[3] = 0x04
-data[4] = 0x05
-packing = "C*"
+data[0] = echo_data
+packing = "a" + echo_data.length.to_s
 
 #SM Send via UUT PAYLOAD_WRITE
 click_cmd(CMD_PL_ECHO, data, packing)
