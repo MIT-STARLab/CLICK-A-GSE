34131832ebb85726cac678243fb5bbcb8e93d529
unknown <CLICK@AA-8VGW373.mit.edu>
2021-01-15 14:53:02 -0500

    save local files

---
+++ procedures/test/payload_cmd.rb
@@ -0,0 +1,44 @@
+load 'crc16.rb'
+
+class PayloadCmd
+
+  # CCSDS constants
+  IDX_CCSDS_VER = 0
+  IDX_CCSDS_APID = 1
+  IDX_CCSDS_GRP = 2
+  IDX_CCSDS_SEQ = 3
+  IDX_CCSDS_LEN = 4
+  IDX_TIME_SEC = 5
+  IDX_TIME_SUBSEC = 6
+  IDX_RESERVED = 7
+
+  CCSDS_VER = 0x18 # ver = 000b, type = 1b (cmd), sec hdr = 1b (yes)
+  CCSDS_GRP_NONE = 0xC0 # grouping = 11b
+
+
+  def send(payload_apid, data)
+    length = data.length
+    pkt = []
+    
+    # construct a CCSDS Header
+    head = []
+    head[IDX_CCSDS_VER] = CCSDS_VER | (payload_apid >> 8)
+    head[IDX_CCSDS_APID] = payload_apid & 0xFF
+    head[IDX_CCSDS_GRP] = CCSDS_GRP_NONE
+    head[IDX_CCSDS_SEQ] = 0
+    head[IDX_CCSDS_LEN] = length - 1  + 2 # - 1 + 2 byte CRC 
+    
+    # pack header the S> means format the short as big endian
+    data = head.pack("C4S>") + data.pack("C*")
+    
+    # add CRC
+    if data.length > 0
+      pkt = data 
+      pkt += [Crc16.new.update(data.unpack("C*"))].pack("S>")
+    end
+
+    pkt = pkt.unpack("C*")
+    cmd("UUT PAYLOAD_WRITE with RAW_BYTES #{pkt}, LENGTH #{pkt.length}")
+  end
+  
+end
---
+++ procedures/test/test_reprogramming.rb
@@ -0,0 +1,38 @@
+load 'payload_cmd.rb'
+set_line_delay(0.0)
+
+# Make sure payload is powered off
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 27, STATUS 0")
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 22, STATUS 0")
+
+# Disable time of tone packets
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 28, STATUS 0")
+
+# Turn payload power on, keep Pi reset active
+wait(2)
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 4, STATUS 1")
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 22, STATUS 1")
+
+# Short wait for VNC2 boot
+wait(1)
+
+# Release Pi reset
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 27, STATUS 1")
+
+# Send empty reprogramming init SPI command to VNC2
+PayloadCmd.new.send(0x7EF, [])
+
+# Wait for VNC2 to complete reset sequence into usb bootloader mode
+wait(15)
+
+# Send second reprogramming init command to VNC2
+PayloadCmd.new.send(0x7EF, [])
+
+# Wait for VNC2 to transmit the embedded first stage bootloader
+wait(5)
+
+# Re-enable time of tone packets
+cmd("UUT SET_LOAD_SWITCH with SWITCH_NUM 28, STATUS 1")
+
+# Start golden image transmission
+cmd("UUT PYLD_CFDP_DL_FILE with PKT_LEN 62, CCSDS_AP_ID 496, TRN_ID 17, SRC_PATH /mnt/sd/general/click_golden.img")
